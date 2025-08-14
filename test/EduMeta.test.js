1a8fe1990c5aa9430407614a3c3cfa39cdbcec14
Suho Kwon <suho.kwon@gmail.com>
2025-08-14 16:07:32 +0900

    Add changes.

--- test/EduMeta.test.js
+++ test/EduMeta.test.js
@@ -1,5 +1,6 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { expect } from "chai";
+import pkg from "hardhat";
+const { ethers } = pkg;
 
 describe("EduMeta Token", function () {
   let EduMeta;
